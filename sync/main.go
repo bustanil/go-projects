a65281b777e140b1e94dcc691c4d3f697c24157d
Bustanil Arifin <bustanil@protonmail.com>
2024-09-06 21:40:25 +0800

    write chunked file

--- sync/main.go
+++ sync/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"os"
-	"slices"
 )
 
-type File struct {
+type ChunkedFile struct {
 	Name   string
 	Size   int64
 	Chunks []Chunk
@@ -16,9 +16,10 @@ type File struct {
 type Chunk struct {
 	Number int
 	Data   []byte
+	Hash   string
 }
 
-func chunkFile(filename string) (*File, error) {
+func chunkFile(filename string, keepData bool) (*ChunkedFile, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
@@ -35,54 +36,75 @@ func chunkFile(filename string) (*File, error) {
 		return nil, err
 	}
 
-	f := &File{
+	f := &ChunkedFile{
 		Name:   stat.Name(),
 		Size:   stat.Size(),
 		Chunks: make([]Chunk, 0),
 	}
 
 	chunkSize := 4 * 1024 * 1024 // 4 MB
-	chunk := make([]byte, 0)
-
-	bufferSize := 2 * 1024 * 1024
-	buffer := make([]byte, bufferSize)
+	chunk := make([]byte, chunkSize)
 
 	i := 0
-	var bytesRead int
 	for {
-		bytesRead, err = file.Read(buffer)
-		chunk = slices.Concat(chunk, buffer[:bytesRead])
+		_, err = file.Read(chunk)
 		if err != nil && err == io.EOF {
 			fmt.Println(err)
 			break
 		}
 
 		if len(chunk) == chunkSize {
+			var data []byte
+			if keepData {
+				data = chunk
+			}
 			f.Chunks = append(f.Chunks, Chunk{
 				Number: i,
-				Data:   chunk,
+				Data:   data,
+				Hash:   fmt.Sprintf("%x", sha256.Sum256(chunk)),
 			})
 			if err != nil {
 				return nil, err
 			}
 
 			i = i + 1
-			bytesRead = 0
-			chunk = make([]byte, 0)
+			chunk = make([]byte, chunkSize)
 		}
 
 	}
 
+	var data []byte
+	if keepData {
+		data = chunk
+	}
 	f.Chunks = append(f.Chunks, Chunk{
 		Number: i,
-		Data:   chunk,
+		Data:   data,
+		Hash:   fmt.Sprintf("%x", sha256.Sum256(chunk)),
 	})
 
 	return f, nil
 }
 
+func writeFile(f *ChunkedFile, outputPath string) error {
+	file, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for _, chunk := range f.Chunks {
+		_, err := file.Write(chunk.Data)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func main() {
-	f, err := chunkFile("/home/bustanil/Downloads/20240831_094558.mp4")
+	f, err := chunkFile("/home/bustanil/Downloads/20240831_094558.mp4", true)
 	//f, err := chunkFile("/home/bustanil/wp-config.php")
 	if err != nil {
 		fmt.Println(err)
@@ -93,6 +115,11 @@ func main() {
 
 	fmt.Println("chunks:", len(f.Chunks))
 	for _, chunk := range f.Chunks {
-		fmt.Println(chunk.Number, len(chunk.Data))
+		fmt.Println(chunk.Number, len(chunk.Data), chunk.Hash)
+	}
+
+	err = writeFile(f, "/home/bustanil/Downloads/20240831_094558_out.mp4")
+	if err != nil {
+		fmt.Println(err)
 	}
 }
